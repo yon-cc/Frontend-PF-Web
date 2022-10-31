0260d288b955167c51b4ad0c6fe550a532d72f3b
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2022-10-31 10:46:44 -0500

    Estructura del proyecto y Header
    Carpetas y archivos del proyecto, ademas, se implemento el header, pero sin su funcionalidad.

---
+++ src/View/Pages/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../Components/header";
+
+export default class Home extends React.Component{
+    render(){
+        return(<>
+            <Header></Header>
+        </>)
+    }
+}
---
+++ src/View/view.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "./Pages/home";
+
+export default class View extends React.Component{
+    render(){
+        return(
+            <Home></Home>
+        )
+    }
+}
