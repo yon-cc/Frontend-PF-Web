f7b19f1111eb39dae17c0178e565a8fee6d6ef8f
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2022-10-31 22:24:31 -0500

    Sidebar
    Sidebar excepto funcionalidad.

--- src/View/Components/sidebar.js
+++ src/View/Components/sidebar.js
@@ -4,9 +4,27 @@ import './sidebar.css';
 export default class Sidebar extends React.Component{
     render(){
         return(
-        <>
-        
-        </>
+        <div className='box'>
+            <div className='filter'>
+                <h2>Precio</h2>
+                <form >
+                    <input type="range" min="1" max="100" value='1' id='' />
+                    <div className='limit left'>
+                        <label for='lower'>Desde</label><br></br>
+                        <input readOnly="readonly" type="text" className='filter-limit' id="" placeholder={`Pedir a la bd`} ></input>
+
+                    </div>
+
+                    <div className='limit right'>
+                        <label for='higher'>De</label><br></br>
+                        <input readOnly="readonly" type="text" className='filter-limit' id="" placeholder={`Pedir a la bd`} ></input>
+
+                    </div>
+
+                </form>
+
+            </div>
+        </div>
         )
     }
 }
