a2973f75b27aa96743fea8b72297927289c2f0c4
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2022-11-03 10:20:08 -0500

    json pruebas

--- src/Controller/controller.js
+++ src/Controller/controller.js
@@ -1,16 +1,26 @@
 export default class Controller{
     constructor(){
-        this.url  = "http://localhost";
+        // ! DEJAR UN SOLO URL y modificar metodos de get y post
+        this.url = "https://raw.githubusercontent.com/yon-cc/Frontend-PF-Web/main/src/Model";
+        // this.urlMax = "";
     }
 
-    async getData(stateData){
-        const response  = fetch(this.url);
+    async getRequest(endpoint){
+        const response  = fetch(this.url + `${endpoint}`);
         const data = (await response).json();
 
+        return data;
+    }
+
+    async getProducts(stateData){
+        const data = await this.getRequest("/data.json?token=GHSAT0AAAAAABY3PUSEVVPKZI6NAYIS324YY3D4N3Q");
+
         stateData = data.slice(0,data.lenght);
+    }
 
-        
+    async getMaxMin(stateMaxMin){
+        const data = await this.getRequest("");
 
-        // return data;
+        stateMaxMin = data.slice(0, data.lenght);
     }
 }
